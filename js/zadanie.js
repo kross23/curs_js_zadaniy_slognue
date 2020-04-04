7037360ed7706f5d397bdbcf23f26676977ed268
kross <662242@mail.ru>
2020-04-04 14:25:03 +0500

    fix:day  lesson07

--- js/zadanie.js
+++ js/zadanie.js
@@ -6,22 +6,23 @@ let d = document;
 let date = new Date();
 let day = date.getDay();
 //debugger;
-day-=1;
+day-=1 ;
 console.log(day);
 
 day+='';
   for(let i in arrWeek){
     let tex = document.querySelector("#ol"); //получаем обьект по ID
     let p = document.createElement("LI");    // создаем элемент списка
-      if(i ==='6'||i==='7') {                // если индекс массива и номер выходных 
+      if(arrWeek[i] ==='Суббота' || arrWeek[i] ==='Воскресенье') {                // если индекс массива и номер выходных 
         p.classList.add('weekend');          // присваивам класс 
-    }else if(i === day){                     // если индекс совпадает с текушим днем то присваиваем класс
-      p.classList.add('curent');
-    }
+        }
+     if(i === day){                     // если индекс совпадает с текушим днем то присваиваем класс
+       p.classList.add('curent');
+     }
   p.innerHTML = arrWeek[i];                  // вкладываем в элемент содержимое массива    
   tex.appendChild(p);                        // вставлям в страницу
   }
 };
+
 day();
 
-//элемент.insertAdjacentText('beforeEnd', arrWeek[i]);
