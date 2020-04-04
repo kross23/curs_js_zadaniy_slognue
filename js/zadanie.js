f73b12f439206227ed2176bbf1da4e01ef16cfbf
kross <662242@mail.ru>
2020-04-04 14:32:46 +0500

    fix:day  lesson07

--- js/zadanie.js
+++ js/zadanie.js
@@ -1,28 +1,27 @@
 'use strict';
 
-const day=()=>{
-let arrWeek=['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-let d = document;
-let date = new Date();
-let day = date.getDay();
-//debugger;
-day-=1 ;
-console.log(day);
+const day = () => {
+  let arrWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  let d = document;
+  let date = new Date();
+  let day = date.getDay();
+  //debugger;
+  day -= 1;
+  console.log(day);
 
-day+='';
-  for(let i in arrWeek){
+  day += '';
+  for (let i in arrWeek) {
     let tex = document.querySelector("#ol"); //получаем обьект по ID
-    let p = document.createElement("LI");    // создаем элемент списка
-      if(arrWeek[i] ==='Суббота' || arrWeek[i] ==='Воскресенье') {                // если индекс массива и номер выходных 
-        p.classList.add('weekend');          // присваивам класс 
-        }
-     if(i === day){                     // если индекс совпадает с текушим днем то присваиваем класс
-       p.classList.add('curent');
-     }
-  p.innerHTML = arrWeek[i];                  // вкладываем в элемент содержимое массива    
-  tex.appendChild(p);                        // вставлям в страницу
+    let p = document.createElement("LI"); // создаем элемент списка
+    if (arrWeek[i] === 'Суббота' || arrWeek[i] === 'Воскресенье') { // если индекс массива и номер выходных 
+      p.classList.add('weekend'); // присваивам класс 
+    }
+    if (i === day) { // если индекс совпадает с текушим днем то присваиваем класс
+      p.classList.add('curent');
+    }
+    p.innerHTML = arrWeek[i]; // вкладываем в элемент содержимое массива    
+    tex.appendChild(p); // вставлям в страницу
   }
 };
 
 day();
-
