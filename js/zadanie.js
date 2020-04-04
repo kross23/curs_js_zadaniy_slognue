2cfcc6f054bc886db5a4065a5ff2aaf19f595afb
kross <662242@mail.ru>
2020-04-04 15:08:46 +0500

    fix:day  lesson07

--- js/zadanie.js
+++ js/zadanie.js
@@ -1,27 +1,35 @@
 'use strict';
 
 const day = () => {
-  let arrWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  let arrWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
   let d = document;
   let date = new Date();
   let day = date.getDay();
   //debugger;
-  day -= 1;
   console.log(day);
 
-  day += '';
-  for (let i in arrWeek) {
-    let tex = document.querySelector("#ol"); //получаем обьект по ID
-    let p = document.createElement("LI"); // создаем элемент списка
-    if (arrWeek[i] === 'Суббота' || arrWeek[i] === 'Воскресенье') { // если индекс массива и номер выходных 
-      p.classList.add('weekend'); // присваивам класс 
+
+  for (let i = 1; i < arrWeek.length; i++) {
+    let tex = document.querySelector("#ol");             //получаем обьект по ID
+    let p = document.createElement("LI");                // создаем элемент списка
+    if (arrWeek[i] === 'Суббота') {                     // если индекс массива и номер выходных 
+      p.classList.add('weekend');                        // присваивам класс 
     }
-    if (i === day) { // если индекс совпадает с текушим днем то присваиваем класс
+    if (i === day) {                                    // если индекс совпадает с текушим днем то присваиваем класс
       p.classList.add('curent');
     }
-    p.innerHTML = arrWeek[i]; // вкладываем в элемент содержимое массива    
-    tex.appendChild(p); // вставлям в страницу
+    p.innerHTML = arrWeek[i];                          // вкладываем в элемент содержимое массива    
+    tex.appendChild(p);                                // вставлям в страницу
   }
+  let tex = document.querySelector("#ol");             //получаем обьект по ID
+  let p = document.createElement("LI");
+  p.classList.add('weekend');                          // присваивам класс 
+
+  if (0 === day) {                                     // если индекс совпадает с текушим днем то присваиваем класс
+    p.classList.add('curent');
+  }
+  p.innerHTML = arrWeek[0];                            // вкладываем в элемент содержимое массива    
+  tex.appendChild(p);
 };
 
 day();
